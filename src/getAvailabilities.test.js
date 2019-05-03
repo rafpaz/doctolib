a3e8ae383c886650554621a1f1b8330a868c78c1
Rafael Paz <rafael@adikteev.com>
2019-05-03 18:22:40 +0200

    add specs to api

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -88,4 +88,37 @@ describe('getAvailabilities', () => {
       expect(availabilities[6].slots).toEqual([]);
     });
   });
+
+  describe('Different number of days ranges', () => {
+    beforeEach(async () => {
+      await knex('events').insert([
+        {
+          kind: 'opening',
+          starts_at: new Date('2014-08-04 09:30'),
+          ends_at: new Date('2014-08-04 12:30'),
+          weekly_recurring: true,
+        },
+        {
+          kind: 'appointment',
+          starts_at: new Date('2014-08-11 10:30'),
+          ends_at: new Date('2014-08-11 11:30'),
+        },
+      ]);
+    });
+
+    it('when ', async () => {
+      const availabilities = await getAvailabilities(new Date('2014-08-10'));
+      expect(availabilities.length).toBe(7);
+
+      expect(String(availabilities[0].date)).toBe(
+        String(new Date('2014-08-10')),
+      );
+      expect(availabilities[0].slots).toEqual([]);
+
+      expect(String(availabilities[1].date)).toBe(
+        String(new Date('2014-08-11')),
+      );
+      expect(availabilities[6].slots).toEqual([]);
+    });
+  });
 });
